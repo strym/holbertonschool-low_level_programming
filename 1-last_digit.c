eb0064b7562b20bcfb7d62f008bbfc86f186b037
strym <andresaguirre104@gmail.com>
2023-11-24 14:56:31 -0800

    arreglado

---
+++ 1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n = 0;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+   if (n==0
+       )
+  return (0);
+wq
+
